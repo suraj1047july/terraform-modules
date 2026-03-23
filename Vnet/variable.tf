2315362a8fc9c7783fb710afbda0faf62b8a5df7
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-03-23 11:18:20 +0530

    Update variable.tf

--- Vnet/variable.tf
+++ Vnet/variable.tf
@@ -1,10 +1,20 @@
-variable "name" {}
-variable "location" {}
-variable "resource_group_name" {}
+variable "name" {
+  type = string
+  description = "give the name of vnet"
+}
+variable "location" {
+  type = string
+  description = "location name "
+}
+variable "resource_group_name" {
+  type = string
+  description = "give the rg name"
+}
 variable "address_space" {
-  type = list(string)
+  type = string
+  description ="give the CIDR range"
 }
 variable "tags" {
-  type    = map(string)
-  default = {}
+  type    = string
+  description ="give the tag"
 }
