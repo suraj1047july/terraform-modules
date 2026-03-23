154f8063024a8b1de97d62cb8cf94c448a4c697c
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-03-23 16:28:14 +0530

    Update output.tf

--- Vnet/output.tf
+++ Vnet/output.tf
@@ -1,5 +1,4 @@
-variable "name"                { type = string }
-variable "location"            { type = string }
-variable "resource_group_name" { type = string }
-variable "address_space"       { type = list(string) }
-variable "tags"                { type = map(string) }
+output "vnet_id"  { 
+    value = azurerm_virtual_network.this.id 
+    }
+
