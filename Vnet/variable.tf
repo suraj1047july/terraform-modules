be47263df40d87fc5976c80419aae611d3dc0a82
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-03-23 16:28:26 +0530

    Update variable.tf

--- Vnet/variable.tf
+++ Vnet/variable.tf
@@ -1,18 +1,5 @@
-variable "name" {
-  type = string
-  description = "give the name of vnet"
-}
-variable "location" {
-  type = string
-  description = "location name "
-}
-variable "resource_group_name" {
-  type = string
-  description = "give the rg name"
-}
-variable "address_space" {
-  type = list(string)
-}
-variable "tags" {
-  type = map(string)
-}
+variable "name"                { type = string }
+variable "location"            { type = string }
+variable "resource_group_name" { type = string }
+variable "address_space"       { type = list(string) }
+variable "tags"                { type = map(string) }
