c244b26173137dd6db7d051d65dae855299bdd66
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-03-23 11:01:45 +0530

    Delete Route-Table directory

--- Route-Table/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route_table" "rt" {
-  vpc_id = var.vpc_id
-}
-
-resource "aws_route" "default" {
-  route_table_id         = aws_route_table.this.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = var.gateway_id
-}
