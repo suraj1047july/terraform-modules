6432f1011a9471789547b188c0aca82cf0bc83a2
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-07-24 14:58:24 +0530

    Add output for Vnet name in output.tf

--- Vnet/output.tf
+++ Vnet/output.tf
@@ -1,4 +1,8 @@
 output "vnet_id"  { 
+    description = "Vnet id"
     value = azurerm_virtual_network.this.id 
     }
-
+output "vnet_name" {
+  description = "Vnet Name"
+  value       = azurerm_resource_group.this.name
+}
