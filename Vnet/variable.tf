ceda82d93f88d8b72132f60ca1633268d4dfc073
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-07-24 16:31:12 +0530

    Rename resource_group variable to resource_group_name

--- Vnet/variable.tf
+++ Vnet/variable.tf
@@ -1,5 +1,5 @@
 variable "name"                { type = string }
 variable "location"            { type = string }
-variable "resource_group"      { type = string }
+variable "resource_group_nmae"      { type = string }
 variable "address_space"       { type = list(string) }
 
