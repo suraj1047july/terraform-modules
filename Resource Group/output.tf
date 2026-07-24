8f8e3b242647086b167be9dd8b3e9077eacbd2a3
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-07-24 14:56:25 +0530

    Update output.tf

--- Resource Group/output.tf
+++ Resource Group/output.tf
@@ -1,3 +1,14 @@
-output "rg_id"  { 
-    value = azurerm_resource_group.this.id 
-    }
+output "rg_name" {
+  description = "Resource Group Name"
+  value       = azurerm_resource_group.this.name
+}
+
+output "rg_id" {
+  description = "Resource Group ID"
+  value       = azurerm_resource_group.this.id
+}
+
+output "location" {
+  description = "Resource Group Location"
+  value       = azurerm_resource_group.this.location
+}
