ee39d8587a619caf5b679e632d5059b65dc5bc8d
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-07-24 14:59:11 +0530

    Add output for Vnet location in output.tf

--- Vnet/output.tf
+++ Vnet/output.tf
@@ -6,3 +6,8 @@ output "vnet_name" {
   description = "Vnet Name"
   value       = azurerm_resource_group.this.name
 }
+output "vnet_location" {
+  description = "Vnet location"
+  value       = azurerm_resource_group.this.location
+}
+
