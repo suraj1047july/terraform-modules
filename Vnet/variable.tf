10406b30c021d4960115888984fcd756914b876d
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-07-24 16:31:37 +0530

    Fix typo in resource_group_name variable

--- Vnet/variable.tf
+++ Vnet/variable.tf
@@ -1,5 +1,5 @@
 variable "name"                { type = string }
 variable "location"            { type = string }
-variable "resource_group_nmae"      { type = string }
+variable "resource_group_name"      { type = string }
 variable "address_space"       { type = list(string) }
 
